c723d01931a5e0543214a73160fcbbb9170e404c
Oliver Fuerst <ofuerst@tsak.net>
2017-08-24 13:10:43 +0100

    feat: add concurrent csv writer

---
+++ csv_writer.go
@@ -0,0 +1,56 @@
+// A "thread" safe way of writing to CSV files
+package ccsv
+
+import (
+	"sync"
+	"encoding/csv"
+	"os"
+)
+
+type CsvWriter struct {
+	mutex     *sync.Mutex
+	csvWriter *csv.Writer
+	file      *os.File
+}
+
+// NewCsvWriter creates a CSV file and returns a CsvWriter
+func NewCsvWriter(fileName string) (*CsvWriter, error) {
+	csvFile, err := os.Create(fileName)
+	if err != nil {
+		return nil, err
+	}
+	w := csv.NewWriter(csvFile)
+	return &CsvWriter{csvWriter: w, mutex: &sync.Mutex{}, file: csvFile}, nil
+}
+
+// Write a single row to a CSV file
+func (w *CsvWriter) Write(row []string) error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	return w.csvWriter.Write(row)
+}
+
+// Write multiple rows to a CSV file
+func (w *CsvWriter) WriteAll(records [][]string) error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	return w.csvWriter.WriteAll(records)
+}
+
+// Flush writes any pending rows
+func (w *CsvWriter) Flush() error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	w.csvWriter.Flush()
+	return w.csvWriter.Error()
+}
+
+// Close CSV file for writing
+// Implicitly calls Flush() before
+func (w *CsvWriter) Close() error {
+	err := w.Flush()
+	if err != nil {
+		return err
+	}
+	return w.file.Close()
+}
